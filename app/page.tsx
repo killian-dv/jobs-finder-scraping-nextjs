f5527e67cbc5692cbf6c8e61c7e2ff4679c10f8a
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-10-22 20:30:25 +0200

    feat : styling for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,50 @@
-import Image from "next/image";
+import { prisma } from "@/lib/prisma";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const jobs = await prisma.jobs.findMany({
+    where: {
+      date: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 1)),
+      },
+    },
+  });
   return (
     <div className="flex flex-col gap-4 max-w-4xl m-auto">
       <h1>Remote Jobs Finder 🔍</h1>
+      <ul className="flex flex-col gap-2">
+        {jobs.map((job) => (
+          <li key={job.id}>
+            <Link href={job.url}>
+              <Card className="hover:bg-muted/50">
+                <CardHeader className="flex flex-row gap-4">
+                  <div>
+                    <Avatar>
+                      <AvatarFallback>{job.company[0]}</AvatarFallback>
+                      {job.logo ? (
+                        <AvatarImage src={job.logo} alt={job.company} />
+                      ) : null}
+                    </Avatar>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <CardTitle>{job.title}</CardTitle>
+                    <CardDescription>
+                      {job.company} - {job.salary}
+                    </CardDescription>
+                  </div>
+                </CardHeader>
+              </Card>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
